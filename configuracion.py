21d43911e8e7c0b3f08d6ab9ea8fa9112c1b2197
Omer21200 <omerbenitez212000@gmail.com>
2025-05-28 21:04:11 -0500

    Base de postgres

--- configuracion.py
+++ configuracion.py
@@ -2,4 +2,8 @@
 #
 # cadena conector a la base de datos
 #
-cadena_base_datos = 'sqlite:///publicaciones.db' 
+#cadena_base_datos = 'sqlite:///publicaciones.db' 
+
+# config.py
+cadena_base_datos = 'postgresql://postgres:oabenitez@localhost:5432/midb'
+
